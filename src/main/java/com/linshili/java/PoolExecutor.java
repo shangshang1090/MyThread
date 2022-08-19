302d624b4860f0a03c94173c2225f5ce8f046235
shangahsng1090 <1090607853@qq.com>
2022-08-19 14:51:43 +0800

    手动创建线程池及两种提交方式

--- src/main/java/com/linshili/java/PoolExecutor.java
+++ src/main/java/com/linshili/java/PoolExecutor.java
@@ -1,2 +1,19 @@
-package com.linshili.java;public class PoolExecutor {
+package com.linshili.java;
+
+import com.sun.jndi.ldap.pool.PooledConnectionFactory;
+
+import java.util.concurrent.*;
+
+public class PoolExecutor {
+    public static void main(String[] args) throws Exception {
+        ThreadPoolExecutor pool = new ThreadPoolExecutor(3, 6, 5, TimeUnit.SECONDS, new ArrayBlockingQueue<>(5),
+                Executors.defaultThreadFactory(), new ThreadPoolExecutor.AbortPolicy());
+        //runnable 方法
+    /*    MyRunnable runnable = new MyRunnable();
+       pool.execute(runnable);*/
+        //callable方法
+        Future<String> submit = pool.submit(new MyCallable(50));
+        String s = submit.get();
+        System.out.println(s);
+    }
 }
